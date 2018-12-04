a0ec8e4c4430fa8911fcd1fa53540488c1e6354f
Isack Munguia <isack.munguia.mejia@gmail.com>
2018-12-04 16:19:51 -0600

    logica para borrar clases

--- src/app/services/pensums.service.ts
+++ src/app/services/pensums.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PensumsService {
 
-  uri = 'http://localhost:3000/api/pensums/';
+  uri = 'http://localhost:3000/api/pensums';
 
   constructor(private http: HttpClient) { }
 
@@ -27,7 +27,33 @@ export class PensumsService {
 
   listarTodasLasAsignaturas() {
     return this
-           .http
-           .get(`${this.uri}`);
-    }
+      .http
+      .get(`${this.uri}`);
+  }
+
+  editarAsignaturaEnPensum(id) {
+    return this
+      .http
+      .get(`${this.uri}/${id}`);
+  }
+
+  updateItem(id_carrera, des_carrera, id_clase, desc_clase, id_requisito, desc_requisito, id) {
+    const obj = {
+      id_carrera: id_carrera,
+      des_carrera: des_carrera,
+      id_clase: id_clase,
+      desc_clase: desc_clase,
+      id_requisito: id_requisito,
+      desc_requisito: desc_requisito
+    };
+    this.http
+      .put(`${this.uri}/${id}`, obj)
+      .subscribe(res => console.log('Done'));
+  }
+
+  deleteItem(id) {
+    return this
+      .http
+      .delete(`${this.uri}/${id}`);
+  }
 }
