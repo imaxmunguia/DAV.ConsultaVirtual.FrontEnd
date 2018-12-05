3413daa1e12e6517b9b35190d982f0550e43bfc9
Jesus Rivera <jesusalberto_rl@unitec.edu>
2018-12-05 15:59:08 -0600

    login

--- src/app/components/dashboard-alumno/dashboard-alumno.component.ts
+++ src/app/components/dashboard-alumno/dashboard-alumno.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/usuario.model';
+import {UsuariosService} from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-dashboard-alumno',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-alumno.component.css']
 })
 export class DashboardAlumnoComponent implements OnInit {
+  user:User;
+  usuariosService:UsuariosService;
+  constructor(usuariosService:UsuariosService) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    
   }
 
 }
